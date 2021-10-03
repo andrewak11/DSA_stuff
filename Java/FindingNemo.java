b9fe5df9c1d00c270ba1569c722e859a1cd3fe79
andrewak11 <andrewakhash.11@gmail.com>
2021-10-03 18:59:34 +0530

    added FindingNemo.java

--- Java/FindingNemo.java
+++ Java/FindingNemo.java
@@ -1,6 +1,23 @@
 package Java;
-class Hello{
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+
+import java.util.Arrays;
+class FindingNemo {
+  public static void main(String[] args) {
+    String[] nemo = new String[] {"Nemo"};
+    String[] everyone = new String[] {"dory","bruce","marlin","nemo","gill","bloat","nigel","squirt","darla","hank"};
+    String[] large = new String[1000];
+    Arrays.fill(large,"Nemo");
+    FindingNemo fn = new FindingNemo();
+    fn.findNemo(large);
+  }
+
+  void findNemo(String [] s){
+    for(int i = 0; i < s.length; i++){
+      if(s[i].equals("Nemo")){
+        System.out.println("Found Nemo!");
+        return;
+      }
     }
+    System.out.println("Nemo not found!");
+  }
 }
